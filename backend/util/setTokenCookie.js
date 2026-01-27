8ad79b8ea247aefaa0d35095fc6b195f0630a8d9
Bholenath Patalay <bholupatalay.1718@gmail.com>
2026-01-27 16:51:48 +0530

    modified settoken and secrettoken files

--- backend/util/setTokenCookie.js
+++ backend/util/setTokenCookie.js
@@ -1,14 +1,14 @@
 const cookieOptions = {
-  withCredentials: true,
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
+  maxAge: 3 * 24 * 60 * 60 * 1000,
 };
 
 module.exports.setTokenCookie = (res, token) => {
   res.cookie("token", token, cookieOptions);
-}
+};
 
 module.exports.clearTokenCookie = (res) => {
   res.clearCookie("token", cookieOptions);
-}
+};
