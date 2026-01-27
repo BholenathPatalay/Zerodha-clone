588051358361fe3ae2ed1e4125bc1a21872912a7
Bholenath Patalay <bholupatalay.1718@gmail.com>
2026-01-27 14:41:27 +0530

    removing options handler

--- backend/index.js
+++ backend/index.js
@@ -24,13 +24,13 @@ const corsOptions = {
     "https://zerodha-dashboard.onrender.com",
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
 
-app.options("/*", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
